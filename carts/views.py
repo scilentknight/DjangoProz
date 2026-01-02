200f1f302a462bfac2eab7345e8d333afcfc9a73
Scilent Knight <scilentknight512@gmail.com>
2026-01-02 19:24:22 +0545

    payments.html page design added

--- carts/views.py
+++ carts/views.py
@@ -222,7 +222,7 @@ def checkout(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        tax = (2 * total)/100
+        tax = (1 * total)/100
         grand_total = total + tax
     except ObjectDoesNotExist:
         pass #just ignore
